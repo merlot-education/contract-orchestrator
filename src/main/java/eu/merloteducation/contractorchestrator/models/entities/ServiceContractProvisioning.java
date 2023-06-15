a76a29bb1105640d7e73e89b62644c3aa9cda0f7
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-15 13:13:41 +0200

    added preliminary class for provisioning of contract

---
+++ src/main/java/eu/merloteducation/contractorchestrator/models/entities/ServiceContractProvisioning.java
@@ -0,0 +1,5 @@
+package eu.merloteducation.contractorchestrator.models.entities;
+
+public class ServiceContractProvisioning {
+    // TODO table with all parameters related to number of exchanges, data transfer parameters that can change during contract lifetime etc...
+}
