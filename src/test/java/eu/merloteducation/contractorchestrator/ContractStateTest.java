855d720f6467b5c8e2e38aaf96a0620c98d4cff6
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-06-16 15:47:13 +0200

    added tests for contract state machine

---
+++ src/test/java/eu/merloteducation/contractorchestrator/ContractStateTest.java
@@ -0,0 +1,85 @@
+package eu.merloteducation.contractorchestrator;
+
+import eu.merloteducation.contractorchestrator.models.entities.ContractState;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ContractStateTest {
+
+    @Test
+    void transitionFromInDraft() {
+        ContractState state = ContractState.IN_DRAFT;
+
+        assertTrue(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertTrue(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertTrue(state.checkTransitionAllowed(ContractState.DELETED));
+
+        assertFalse(state.checkTransitionAllowed(ContractState.REVOKED));
+        assertFalse(state.checkTransitionAllowed(ContractState.RELEASED));
+        assertFalse(state.checkTransitionAllowed(ContractState.ARCHIVED));
+    }
+
+    @Test
+    void transitionFromDeleted() {
+        ContractState state = ContractState.DELETED;
+
+        assertFalse(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertFalse(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertFalse(state.checkTransitionAllowed(ContractState.DELETED));
+        assertFalse(state.checkTransitionAllowed(ContractState.REVOKED));
+        assertFalse(state.checkTransitionAllowed(ContractState.RELEASED));
+        assertFalse(state.checkTransitionAllowed(ContractState.ARCHIVED));
+    }
+
+    @Test
+    void transitionFromConsumerSigned() {
+        ContractState state = ContractState.SIGNED_CONSUMER;
+
+        assertTrue(state.checkTransitionAllowed(ContractState.RELEASED));
+        assertTrue(state.checkTransitionAllowed(ContractState.REVOKED));
+
+        assertFalse(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertFalse(state.checkTransitionAllowed(ContractState.DELETED));
+        assertFalse(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertFalse(state.checkTransitionAllowed(ContractState.ARCHIVED));
+    }
+
+    @Test
+    void transitionFromRevoked() {
+        ContractState state = ContractState.REVOKED;
+
+        assertFalse(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertFalse(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertFalse(state.checkTransitionAllowed(ContractState.DELETED));
+        assertFalse(state.checkTransitionAllowed(ContractState.REVOKED));
+        assertFalse(state.checkTransitionAllowed(ContractState.RELEASED));
+        assertFalse(state.checkTransitionAllowed(ContractState.ARCHIVED));
+    }
+
+    @Test
+    void transitionFromReleased() {
+        ContractState state = ContractState.RELEASED;
+
+        assertTrue(state.checkTransitionAllowed(ContractState.REVOKED));
+        assertTrue(state.checkTransitionAllowed(ContractState.ARCHIVED));
+
+        assertFalse(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertFalse(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertFalse(state.checkTransitionAllowed(ContractState.DELETED));
+        assertFalse(state.checkTransitionAllowed(ContractState.RELEASED));
+    }
+
+    @Test
+    void transitionFromArchived() {
+        ContractState state = ContractState.ARCHIVED;
+
+        assertFalse(state.checkTransitionAllowed(ContractState.IN_DRAFT));
+        assertFalse(state.checkTransitionAllowed(ContractState.SIGNED_CONSUMER));
+        assertFalse(state.checkTransitionAllowed(ContractState.DELETED));
+        assertFalse(state.checkTransitionAllowed(ContractState.REVOKED));
+        assertFalse(state.checkTransitionAllowed(ContractState.RELEASED));
+        assertFalse(state.checkTransitionAllowed(ContractState.ARCHIVED));
+    }
+}
