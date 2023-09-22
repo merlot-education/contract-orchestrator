794685bc3eff79c5fb63c92970f2357fef661e82
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-09-22 09:03:00 +0200

    remove unused import

--- src/main/java/eu/merloteducation/contractorchestrator/models/edc/asset/DataAddress.java
+++ src/main/java/eu/merloteducation/contractorchestrator/models/edc/asset/DataAddress.java
@@ -2,7 +2,6 @@ package eu.merloteducation.contractorchestrator.models.edc.asset;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import eu.merloteducation.contractorchestrator.models.edc.EdcConstants;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
