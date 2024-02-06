5800ea82bf3f4d70e8a17dde62e666816950362d
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-02-06 14:38:56 +0100

    remove unused imports

--- src/main/java/eu/merloteducation/contractorchestrator/service/MessageQueueService.java
+++ src/main/java/eu/merloteducation/contractorchestrator/service/MessageQueueService.java
@@ -15,11 +15,8 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
