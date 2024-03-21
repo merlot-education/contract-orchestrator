b079b10fdffa56fdaf3d7186f06d014353259853
Marc B <125284318+M-Busk@users.noreply.github.com>
2024-03-21 16:45:46 +0100

    update check

--- src/main/java/eu/merloteducation/contractorchestrator/models/entities/ServiceContractProvisioning.java
+++ src/main/java/eu/merloteducation/contractorchestrator/models/entities/ServiceContractProvisioning.java
@@ -35,6 +35,6 @@ public abstract class ServiceContractProvisioning {
     }
 
     public boolean transitionAllowed(ContractState targetState) {
-        return true; // validUntil can be null if selected time is unlimited
+        return targetState != null; // validUntil can be null if selected time is unlimited
     }
 }
